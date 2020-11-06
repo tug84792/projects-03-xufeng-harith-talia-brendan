3bc71098dff8d62b2430ddf10a7d130a19b254fa
BSLisowskiTU <70763386+BSLisowskiTU@users.noreply.github.com>
2020-11-06 12:31:18 -0500

    Update checkExtension.py

--- checkExtension.py
+++ checkExtension.py
@@ -1,14 +1,12 @@
 import os.path
 
-def checkExtension():
-    filename = input('ImageName.jpg\n')
-    while filename != 'q':
-        if filename.endswith('.jpg'):
-            if os.path.isfile(filename):
+def checkExtension(images):
+    for i in images:
+        if i.endswith('jpg'):
+            if os.path.isfile(i):
                 print('File works')
                 #upload file
             else:
                 print('File error')
         else:
             print('File not .jpg')
-        filename = input('ImageName.jpg\n')
