6c141aedcada59b38bff24483e26eaa6ede94406
XufengRen <59923595+XufengRen@users.noreply.github.com>
2020-11-16 19:47:26 -0500

    Add files via upload
    Syntax fixes

--- generate_test_train_and_valid_txt.py
+++ generate_test_train_and_valid_txt.py
@@ -1,17 +1,17 @@
 import os
 
 with open('data/train.txt', 'w')as out:
-    for images in [f for f in os.listdir('train') if f endswith('jpg')]:
+    for images in [f for f in os.listdir('train') if f.endswith('jpg')]:
         out.write('train/' + img + '\n')
 
 import os
 
 with open('data/test.txt', 'w')as out:
-    for images in [f for f in os.listdir('test') if f endswith('jpg')]:
+    for images in [f for f in os.listdir('test') if f.endswith('jpg')]:
         out.write('test/' + img + '\n')
 
 import os
 
 with open('data/valid.txt', 'w')as out:
-    for images in [f for f in os.listdir('valid') if f endswith('jpg')]:
+    for images in [f for f in os.listdir('valid') if f.endswith('jpg')]:
         out.write('valid/' + img + '\n')
