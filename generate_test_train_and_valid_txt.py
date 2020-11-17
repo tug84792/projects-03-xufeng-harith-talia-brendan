e72ed49b09b8b737521992c8aa494420c26bbfd9
XufengRen <59923595+XufengRen@users.noreply.github.com>
2020-11-17 10:59:31 -0500

    Add files via upload

--- generate_test_train_and_valid_txt.py
+++ generate_test_train_and_valid_txt.py
@@ -2,16 +2,16 @@ import os
 
 with open('data/train.txt', 'w')as out:
     for images in [f for f in os.listdir('train') if f.endswith('jpg')]:
-        out.write('train/' + img + '\n')
+        out.write('train/' + images + '\n')
 
 import os
 
 with open('data/test.txt', 'w')as out:
     for images in [f for f in os.listdir('test') if f.endswith('jpg')]:
-        out.write('test/' + img + '\n')
+        out.write('test/' + images + '\n')
 
 import os
 
 with open('data/valid.txt', 'w')as out:
     for images in [f for f in os.listdir('valid') if f.endswith('jpg')]:
-        out.write('valid/' + img + '\n')
+        out.write('valid/' + images + '\n')
