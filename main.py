c9287c8d1c7ab4da6e665548ea1592b2af5ff4d1
XufengRen <59923595+XufengRen@users.noreply.github.com>
2020-11-24 15:37:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,29 @@ import cv2
 import numpy as np
 import time
 import darknet
-import setEmail
+from setEmail import email
+
+#############################
+#Send notification to email
+#############################
+
+############################
+#write detections to log file
+############################
+
+#################################################
+#Rules to decide if an detected object is valid
+#rule 1: confident bigger or equal to 85%?
+#rule 2: 
+#################################################
+
+##################################
+#The main part
+##################################
+def main():
+    
+    configPath = "./cfg/knockknock_cfg.cfg"
+    weightPath = "./knockknock_cfg_best.weights"
+    metaPath = "./cfg/obj.data"
+if __name__ == "__main__":
+  main()
