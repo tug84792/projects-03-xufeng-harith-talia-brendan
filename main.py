1090b55b3c8e3b2895755fe1467c2eee0d26b1a6
XufengRen <59923595+XufengRen@users.noreply.github.com>
2020-11-24 15:57:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ from setEmail import email
 #Send notification to email
 #############################
 
+
+
+
 ############################
 #write detections to log file
 ############################
+
+
+
 
 #################################################
 #Rules to decide if an detected object is valid
@@ -20,13 +26,30 @@ from setEmail import email
 #rule 2: 
 #################################################
 
+
+
+
 ##################################
 #The main part
 ##################################
 def main():
-    
+    #perform detection
     configPath = "./cfg/knockknock_cfg.cfg"
     weightPath = "./knockknock_cfg_best.weights"
     metaPath = "./cfg/obj.data"
+    
+    #decide input source
+    cap = cv2.VideoCapture("")#<-enter input video address here    
+    RTSP_URL = #enter RTSP URL here if using a camear
+    cap = cv2.VedeoCapture(RTSP_URL)
+    
+    #set output video footage
+    
+    #a reuse image for each detection
+    
+    #loop through the video
+        #a detection go through the rule
+        #a detection go to send email and log if pass the fulr
+        
 if __name__ == "__main__":
   main()
